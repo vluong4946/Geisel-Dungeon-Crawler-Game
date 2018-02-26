fb34ee04efc8a527f438087ca642c3347a502f81
parkyw1206 <36143152+parkyw1206@users.noreply.github.com>
2018-02-25 22:53:38 -0800

    Update Dungeon.java

--- Dungeon.java
+++ Dungeon.java
@@ -1,13 +1,25 @@
 import java.util.ArrayList;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBase;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
@@ -50,23 +62,105 @@ int[][] arena = ArenaArray.createArenaArray();
 	private Node human;
 
 	
-	private Node initHuman(){
+	private Node initHuman() {
 		Rectangle human = new Rectangle(10, 10, Color.AQUA);
 		return human;
 	}
 	
-	
-	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+
+
+		GridPane pane = new GridPane();
+		GridPane pane1 = new GridPane();
+			
+		Button bt1,bt2,bt3,bt4;
 		
-		primaryStage.setScene(new Scene(createContent()));
+		Scene scene,scene1;
+		Scene sc = new Scene(createContent());
+		
+		//primaryStage.setScene(new Scene(createContent()));
+
 		primaryStage.centerOnScreen();
 		primaryStage.show();
 		
+	    scene = new Scene(pane,2000,1000);
+	    scene1 = new Scene(pane1, 500,800);
+
+	    primaryStage.setScene(scene);
+
+	    Label label = new Label("Geisel Library Crawl");
+	    label.setTextFill(Color.BROWN);
+	    label.setFont(Font.font("MONOSPACED", FontWeight.BOLD, 80));
+	    
+		bt1 = new Button();
+		bt2 = new Button();
+		bt3 = new Button();
+		bt4 = new Button();
 		
-		primaryStage.getScene().setOnKeyPressed(e -> {
-			switch (e.getCode()) {
+		bt1.setText("Create New Account");
+		bt2.setText("WORKING"); // Call your file
+		bt3.setText("WORKING"); // See the rank
+		bt4.setText("WORKING"); // quit
+		
+	    pane.getChildren().add(label);
+	    
+	    pane.add(bt1,0,8);
+	    pane.add(bt2, 0, 10);
+	    pane.add(bt3, 5, 8);
+	    pane.add(bt4, 5, 10);
+	    pane.setPadding(new Insets(180,100,100,500));
+		
+	    pane.setVgap(10);
+		pane.setHgap(10);
+	    
+	    bt1.setOnAction((ActionEvent e) -> {
+	   	
+	    if(e.getSource() == bt1)
+			primaryStage.setScene(scene1);
+		});
+	    
+		pane1.setVgap(10);
+		pane1.setAlignment(Pos.CENTER);
+		pane1.setPadding(new Insets(25, 25, 25, 25));
+		
+		Text title = new Text("Hi Hero, Make your account");
+		title.setFont(Font.font("MONOSPACED", FontWeight.BOLD, 20));
+		//title.setFill(Color.BLACK);
+		
+		pane1.add(title, 0, 0 , 2 , 1);
+		
+		Label userName = new Label("User Name: ");
+		pane1.add(userName, 0, 1);
+		
+		TextField userTextField = new TextField();
+		pane1.add(userTextField, 1, 1);
+
+		Label password = new Label("Password: ");
+		pane1.add(password, 0, 2);
+		
+		PasswordField pwField = new PasswordField();
+		pane1.add(pwField, 1, 2);
+		
+		Button btn = new Button("Login");
+		HBox hbBtn = new HBox(10);
+		hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+		hbBtn.getChildren().add(btn);
+		pane1.add(hbBtn, 1, 4);
+		
+		final Text actiontarget = new Text();
+		pane1.add(actiontarget,1, 6);
+	    pane.setStyle("-fx-background-color: yellow;-fx-padding: 10px;");
+//	    pane1.setStyle("-fx-background-color: red;-fx-padding: 10px;");
+		
+    
+	    btn.setOnAction((ActionEvent e)->{
+	    	if(e.getSource() == btn)
+	    		primaryStage.setScene(sc);
+	    
+				
+		sc.setOnKeyPressed(ke -> {
+			switch (ke.getCode()) {
 			case W:
 				//if(ArenaArray.getAbove()!=WALL) {
 				//	if(ArenaArray.getAbove()==ENEMY) {
@@ -130,14 +224,11 @@ int[][] arena = ArenaArray.createArenaArray();
 				break;
 			}
 		});
-		
-		
-		
-		
-	}
+	    });
 
 	
 	
+}
 	public static void main(String[] args) {
 		launch(args);
 	}	

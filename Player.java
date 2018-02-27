a3a11c47f6dd418cf2336566ff29665db3fa8dad
IcanDXD <36774788+IcanDXD@users.noreply.github.com>
2018-02-26 18:30:28 -0800

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,5 +1,5 @@
 import java.util.Arrays;
-
+import java.util.Random;
 public class Player {
 	//fields for a player instance
 	private int health;
@@ -24,12 +24,88 @@ public class Player {
 	
 	
 	class Item{
-		class Potion extends Item{
-			
+		Random rng = new Random();
+		int item_value;
+		public Item() {
+			switch(rng.nextInt(3)) {
+				case 0:
+				case 1:
+					Item potion = new Potion();
+					break;
+				case 2:
+					Item weapon = new Weapon();
+					break;
+			}
 		}
-		//... more Item objects to come
-	}
-
-	
+		class Potion extends Item{
+			public int potionEncountered() {
+			switch (rng.nextInt(10)) {
+				case 0: 
+				case 1:
+				case 2:
+					System.out.println("You have recieved a Starbuck Mocha(small health potion)");
+					item_value = 0;
+					break;
+				case 3:
+				case 4:
+					System.out.println("You have recieved a redbull (Mediom health potion)");
+					item_value = 1;
+					break;
+				case 5:
+					System.out.println("You have recieved a DoubleShot Expresso (Large health potion)");
+					item_value = 2;
+					break;
+				case 6:
+				case 7:
+					System.out.println("You have recieved Doritos (Small strength potion)");
+					item_value = 3;
+					break;
+				case 8:
+					System.out.println("You recieve a family size bag of Muchies (Large strength potion)");
+					item_value = 4;
+					break;
+				case 9:
+					System.out.println("Whilst reaching for the mysterious, you trip and fall, subsequently crushing the item ( -5 health (good job -_-))");
+					item_value = 5;
+					break;
+			}
+			return item_value;
+			}
+				class Weapon extends Item{
+					public int weaponEncountered() {
+					switch (rng.nextInt(6)) {
+					case 0:
+					case 1:
+						System.out.println("You have recieved a #2 pencil ( Weak overall stat weapon)");
+						item_value = 6;
+						break;
+					case 2:
+						System.out.println("You have recieved an ascii cheat sheet for all programing languages");
+						item_value = 7;
+						break;
+					case 3:
+						System.out.println("You have recieved a Texas Instrument Ti- 89 ( Strong Math stat weapon)");
+						item_value = 8;
+						break;
+					case 4:
+						System.out.println("You have recieved a set of Princeton Series 7050 Kolinsky Sable Round paint brushes ( Strong Fine-Arts stat weapon)");
+						break;
+					case 5:
+						System.out.println("You have recieved Mac ios Air ( Medium overall stat weapon)");
+						break;
+					}
+					return item_value;
+					
+				}
+				}
+		public void storingItem() {
+			
+			
+			}
+				
+			}
+		}
+			
+		
 
 }

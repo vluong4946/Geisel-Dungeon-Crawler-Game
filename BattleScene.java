a45767147c06ee0bef13a7ae38e2d439d8b84c3b
BananaInTheSky <36524100+BananaInTheSky@users.noreply.github.com>
2018-03-12 15:33:55 -0700

    Update BattleScene.java

--- BattleScene.java
+++ BattleScene.java
@@ -1,4 +1,3 @@
-
 import javafx.scene.control.*;
 import javafx.scene.shape.*;
 import javafx.scene.layout.*;
@@ -14,19 +13,24 @@ public class BattleScene{
 	static Button attack = new Button("ATTACK");
 	static Button run = new Button("RUN");
 	static Button skill = new Button("SKILL");
-	static Button item = new Button("ITEM");
-	static Label pHealth = new Label("Health");
+	static Button item = new Button("DRINK COFFEE");
+	static Label pHealth = new Label("Your Health");
 	static Label pEnergy = new Label("Energy");
-	static Label eHealth = new Label("Health");
-	static GameGui gui;
+	static Label eHealth = new Label("Enemy Health");
 
 	//Create rectangles that represent resource bars
 	//variables replaced with resources
 
 	//these variables should equal values gotten from the player and monster class
-	static int PLAYER_HEALTH=100;
-	static int PLAYER_ENERGY=100;
+	static int PLAYER_HEALTH;
+	static int PLAYER_ENERGY;
 	static int ENEMY_HEALTH=100;
+	
+	static final int DEFAULT_ATK_DMG = 25;
+	static final int SKILL_COST = 10;
+	static final int SCORE_REWARD = 50;
+	static final int COFFEE_HEAL_AMOUNT = 40;
+	static String MAJOR;
 
 	static int PLAYER_ATTACK;
 	static int ENEMY_ATTACK;
@@ -35,21 +39,54 @@ public class BattleScene{
 	static Rectangle playerEnergy;
 	static Rectangle enemyHealth;
 
+	Minion enemy;
 	//Adds color strip to bottom
 	static Rectangle strip = new Rectangle(GameGui.P_STAGE_SIZE,200);
 
 	//imports images for the enemies and players
-	static Image triton = new Image("Triton.png");
+	static Image triton = new Image("Triton.jpg");
 	static ImageView tritonIV = new ImageView();
 	
-	public static Scene battleStart(/*pass in a value that identifies enemy*/){
+	public static Scene battleStart(Player player, Minion enemy){ //Pass in enemy also
+		PLAYER_HEALTH= player.health;
+		PLAYER_ENERGY= player.energy;
+		ENEMY_HEALTH=100;
+
+		switch(player.major) {
+		case "Computer Science": 
+			PLAYER_ATTACK = DEFAULT_ATK_DMG - enemy.MonsterResistence[Monster.CS_INDEX]; 
+			ENEMY_ATTACK = enemy.MonsterAttackValue[Monster.CS_INDEX];
+			break;
+		case "Mathmatics": 
+			PLAYER_ATTACK = DEFAULT_ATK_DMG - enemy.MonsterResistence[Monster.MATH_INDEX]; 
+			ENEMY_ATTACK = enemy.MonsterAttackValue[Monster.MATH_INDEX];
+			break;
+		case "Art": 
+			PLAYER_ATTACK = DEFAULT_ATK_DMG - enemy.MonsterResistence[Monster.FINE_ARTS_INDEX]; 
+			ENEMY_ATTACK = enemy.MonsterAttackValue[Monster.FINE_ARTS_INDEX];
+			break;
+		case "History": 
+			PLAYER_ATTACK = DEFAULT_ATK_DMG - enemy.MonsterResistence[Monster.FINE_ARTS_INDEX]; 
+			ENEMY_ATTACK = enemy.MonsterAttackValue[Monster.FINE_ARTS_INDEX];
+			break; 
+		case "Psychology": 
+			PLAYER_ATTACK = DEFAULT_ATK_DMG - enemy.MonsterResistence[Monster.FINE_ARTS_INDEX]; 
+			ENEMY_ATTACK = enemy.MonsterAttackValue[Monster.FINE_ARTS_INDEX];
+			break;
+		default: 
+			PLAYER_ATTACK = DEFAULT_ATK_DMG; 
+			ENEMY_ATTACK = enemy.MonsterAttackValue[Monster.FINE_ARTS_INDEX];
+			break;
+		}
 		
+		//ENEMY_ATTACK;
+	
 		
 		//Assigns actions to the buttons
-		attack.setOnAction(e -> attack());
-		skill.setOnAction(e -> skill());
-		item.setOnAction(e -> item());
-		run.setOnAction(e -> run());
+		attack.setOnAction(e -> attack(player));
+		skill.setOnAction(e -> skill(player));
+		item.setOnAction(e -> drinkCoffee(player));
+		run.setOnAction(e -> run(player));
 		
 		
 		
@@ -64,51 +101,63 @@ public class BattleScene{
 	}
 	
 	//methods for each action
-	public static void attack() {
-		System.out.println("You attack the enemy!");
-		ENEMY_HEALTH-=PLAYER_ATTACK;
-		System.out.println("The enemy attacks you!");
-		enemyAttack();
+	public static void attack(Player player) {
+		System.out.println("You attack the enemy, dealing " + PLAYER_ATTACK + " damage!");
+		ENEMY_HEALTH -= PLAYER_ATTACK;
+		System.out.println("The enemy attacks you, dealing " + ENEMY_ATTACK + " damage!");
+		enemyAttack(player, ENEMY_ATTACK);
 		updateBattle();
 	}
 	
-	public static void skill() {
-		if(PLAYER_ENERGY>=10) {
+	public static void skill(Player player) {
+		if(player.energy>=10) {
 			System.out.println("You spend 10 energy and use a super attack!");
-			ENEMY_HEALTH-=PLAYER_ATTACK*2;
-			PLAYER_ENERGY-=10;
+			ENEMY_HEALTH-=PLAYER_ATTACK*2; //skill does twice normal attack dmg
+			player.energy-=SKILL_COST;
+			PLAYER_ENERGY-=SKILL_COST;
 			System.out.println("The enemy attacks you!");
-			enemyAttack();
+			enemyAttack(player, ENEMY_ATTACK);
 			updateBattle();}
 		else
 			System.out.println("You do not have enough energy!");
 			updateBattle();
 	}
 	
-	public static void item(/*opens bag with items*/) {
+	public static void item(Player player) {
 		
 		
 		updateBattle();
 	}
 	
-	public static void run() {
+	public static void run(Player player) {
 		if (PLAYER_ENERGY>=50) {
 			System.out.println("You try to get away!");
 			PLAYER_ENERGY-=50;
-			updateBattle();}
-		else 
+			player.energy-=50;
+			updateBattle();
+		}
+		else {
 			System.out.println("You do not have enough energy!");
 			updateBattle();
-	}
-	
-	public static void enemyAttack() {
-		PLAYER_HEALTH-=ENEMY_ATTACK;
-		if(PLAYER_HEALTH < 0)
-		{
-			gui.endingButton();
 		}
+			
 	}
 	
+	public static void enemyAttack(Player player, int ENEMY_ATTACK) {
+		player.health-=ENEMY_ATTACK;
+		PLAYER_HEALTH-=ENEMY_ATTACK;
+	}
+	
+	public static void drinkCoffee(Player player) {
+		if(player.coffeeAmount > 0) {
+			player.health += COFFEE_HEAL_AMOUNT;
+			PLAYER_HEALTH += COFFEE_HEAL_AMOUNT;
+			player.energy += COFFEE_HEAL_AMOUNT * 2;
+			PLAYER_ENERGY += COFFEE_HEAL_AMOUNT * 2;
+			player.coffeeAmount -= 1;
+		}
+		
+	}
 	
 	public static void updateBattle(){
 		battlePane.getChildren().clear();
@@ -161,11 +210,12 @@ public class BattleScene{
 		
 		strip.setTranslateY(300);
 		
-		if (ENEMY_HEALTH==0)
+		if (ENEMY_HEALTH <= 0)
 			GameGui.window.setScene(GameGui.scene);
-		if (PLAYER_HEALTH==0)
+		if (PLAYER_HEALTH <= 0)
+			System.exit(0);
 			//send to ending screen
-			GameGui.window.setScene(GameGui.scene); //this is temporary
+			//GameGui.window.setScene(GameGui.scene); this is temporary
 		
 	}
 }
